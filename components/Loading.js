bf304a2a86e9eeb96c72213cc38f4e6e0313c9e1
tpiros <2042718+tpiros@users.noreply.github.com>
2022-08-15 16:08:38 +0800

    gallery

--- components/Loading.js
+++ components/Loading.js
@@ -3,9 +3,7 @@ const Loading = () => {
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <p className="py-6">
-            <progress className="40" max="100"></progress>
-          </p>
+          <p className="py-6">... Loading ...</p>
         </div>
       </div>
     </div>
